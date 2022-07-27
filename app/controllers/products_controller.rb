ff51e005499555de6c6e6e40611dd5c5b3ed7140
Dan Cross <dancross96@gmail.com>
2022-07-27 13:15:00 -0500

    added route for all products and route for just a tv

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+  
+  def all_products
+    render json: {
+      products: Product.all
+  }
+  end
+
+  def tv
+    render json: {
+      product: Product.find_by(name: "TV")
+    }
+  end
+
 end
