c0f999f0b40bdd787f3752b534cfa37778bdd2c6
Dan Cross <dancross96@gmail.com>
2022-08-02 14:25:06 -0500

    added seed dump gem and ran it to backup data in seeds file

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem "puma", "~> 5.0"
 
 gem 'jb'
 
+gem 'seed_dump'
+
 gem 'rack-cors'
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "TV", price: "199.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/9/90/LCD_generic_tv.jpg ", description: "20 inch LCD TV", inventory: nil},
+  {name: "computer", price: "149.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Computer_monitor.jpg/800px-Computer_monitor.jpg", description: "a cool computer that worlks well for high computing power needs", inventory: nil},
+  {name: "Fridge", price: "2000.0", image_url: "https://images.thdstatic.com/productImages/cc115637-8405-4a3a-b390-127951ccad8f/svn/stainless-look-magic-chef-mini-fridges-hmdr450se-64_1000.jpg", description: "keeps food cool", inventory: nil},
+  {name: "blender", price: "50.0", image_url: "https://nbmedia.imgix.net/nb-blender-combo-unit-empty-1500-x-1201-hero.jpg?auto=compress%2Cformat&ixlib=php-3.3.0&h=900", description: "this baby will blend just about anything", inventory: nil},
+  {name: "Sofa", price: "500.0", image_url: "https://www.ikea.com/us/en/images/products/uppland-sofa-hallarp-gray__0818567_pe774489_s5.jpg", description: "The sofa king", inventory: nil},
+  {name: "table", price: "5.0", image_url: "chair.com", description: "a small table surface", inventory: nil},
+  {name: "The Big Recliner", price: "50.0", image_url: "https://havertys.scene7.com/is/image/Havertys/0-3500-1206?op_sharpen=1&wid=768&hei=512&qlt=70", description: "sit back and relax dude", inventory: nil},
+  {name: "huge table", price: "25.0", image_url: "table.com", description: "a huge table surface", inventory: nil},
+  {name: "The Big huge gigantic tabl", price: "100.0", image_url: "table.com", description: "a gigantic huge table surface", inventory: nil}
+])
