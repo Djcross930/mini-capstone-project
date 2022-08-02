b18cdc8063d543e04455d2b4ea13f76cc024aee8
Dan Cross <dancross96@gmail.com>
2022-08-02 11:56:53 -0500

    added index action through jb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json(methods: [:friendly_created_at, :is_discoounted?, :tax, :total])
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
