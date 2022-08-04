8e3b1676a866c775fbe67c092ec56bb2aaa181ab
Dan Cross <dancross96@gmail.com>
2022-08-04 12:30:18 -0500

    added show and index action for supplier

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,12 @@
 class SuppliersController < ApplicationController
+  def index
+    suppliers = Supplier.all
+    render json: suppliers.as_json
+  end
+
+  def show
+    supplier = Supplier.find_by(id: params[:id])
+    render json: supplier.as_json  
+  end
+
 end
